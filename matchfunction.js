e261bd7d9268ab401899450d1eaeeaa963909089
MathiasFaber <makr20ah@student.cbs.dk>
2020-11-25 10:01:48 +0100

    Changes: working with JSNO files

--- matchfunction.js
+++ matchfunction.js
@@ -2,7 +2,11 @@
 var possibleMatch = ["Anders", "Anna"];//Anders (arrayet her skal være tomt, og likes pushes ind i det)
 var possibleMatch2 = ["Anna", "Anders"]; //Anna (arrayet her skal være tomt, og likes pushes ind i det)
 
+// var possibleMatch = window.localStorage.getItem('user');
+// lave flere af disse variable, så appen kan understøtte 10 brugere ish?
+// ikke optimalt men bare for at vise at den her måde fungerer. 
 
+/*
 function like() {
     possibleMatch.push(document.getElementById("profile").innerHTML);
     document.getElementById("like").innerHTML = possibleMatch;
@@ -10,64 +14,38 @@ function like() {
     
 
 
-/*
+
     for (i = 0; i < possibleMatch2.length; i++){
         if (document.getElementById("User1").innerHTML == possibleMatch2 && possibleMatch2 == possibleMatch){
 
         }
-*/
-};
 
+};
+*/
 var matches = [];
 
 function match (){
 
     for (i = 0; i < possibleMatch.length; i++){
-        for (j = 0; j < possibleMatch2.length; j++){
+        for (j = i+1; j < possibleMatch2.length; j++){
             if (possibleMatch[i] == possibleMatch2[j]){
-                matches.push(possibleMatch[0] + " & " +  possibleMatch2[0] + " is a match")
+                matches.push(possibleMatch[i] + " & " +  possibleMatch2[i] + " is a match")
                 break;
             }
         }
     }
-}
-
-console.log(match())
-console.log(matches)
-
-/*
-function like2() {
-    possibleMatch2.push(document.getElementById("profile").innerHTML);
-    document.getElementById("like").innerHTML = possibleMatch2;
-};
-*/
-
-/*
-document.getElementById("User1").innerHTML = User;
-
-function match() {
-    for (i = 0; i < possibleMatch2.length; i++){
-        if (User == possibleMatch2){
-            alert ("It's a match!")
-        }
-
-    }
+    return matches;
+    
 }
 console.log(match())
-*/
 
-//document.getElementById("like").innerHTML = likesArray.push(document.getElementById("profile").innerHTML)
-    /*
-    addEventListener("click", function(){
-        likesArray.push("aLikedPerson");
-        console.log(likesArray)
-        */
-/*
 
-    document.getElementById("dislikeBtn").addEventListener("click", function(){
-        
-    });
-}
-*/
 
-//lav en github!!
+module.exports = match;
+module.exports = matches;
+
+//fs.writefilesync (lav en variabel der henter noget fra input feltet når en bruger oprettes, for at ligge den i en JSON fil.)
+// fx var jsonstring = queryselector...
+//working with JSON website
+
+let requestURL = 
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const server = express()
 const port = 4000
-const profiles = require("./matchfunction")
-const loginControl = require('./controller/loginControl')
+const possibleMatch = require("./matchfunction")
+//const loginControl = require('./controller/loginControl')
+const match = require('./matchfunction')
 
+// Find ud af at hente data fra localstorage
+server.post('/possibleMatch',(req, res)=>{
+  res.send(window.localStorage.getItem('taskList'))
+})
 
-server.post('/login', loginControl)
+// Lav disse post request, så den opretter en bruger (Muligvis hvis man kan pushe det her ind i local storage? idk)
+server.get('/user2', /*ensureToken,  protectedCon"troller,*/ (req, res)=>{
+  res.send("Ny bruger tilføjet: " + possibleMatch);
+})
 
+/*
+//lav de her get requests om til at, man skal hente personer som er oprettet i systemet fra local storage.
+//Dette get request skal altså gøre sådan, at de vises på skærmen, så man enten kan like eller dislike personerne. 
+//Ensuretoken og protectedcontroller tjekker for JWT token, så det bør nok udelades. 
 
+server.get('/user1', ensureToken,  protectedController, (req, res)=> {
+  res.send(HardUser[0]);
+})
+
+// delete requested skal slette en bruger fra local storage.
+server.delete('/user2delete', ensureToken,  protectedController, (req, res)=>{
+  res.send('Har slettet:' + " " + HardUser[1].name);
+})
+
+*/
 server.listen(port, () => {
     console.log(`Server-applikation lytter på http://localhost:${port}`)
   })
+
+
+  // overvej at inkluder jwt token anyways
